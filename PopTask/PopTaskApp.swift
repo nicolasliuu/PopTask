5418d06cffeb205d7c4e77abb9d7f02f5f5361c4
Nicolas Liu <nicolasliu18@gmail.com>
2023-08-12 10:17:53 -0400

    Initial Commit

---
+++ PopTask/PopTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopTaskApp.swift
+//  PopTask
+//
+//  Created by Nicolas Liu on 8/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PopTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
